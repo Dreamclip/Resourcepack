cb398b90ab00096ee6dcf2122ed1b5cf5b0f2fc5
Dreamclip <gggmasterrb@gmail.com>
2025-11-09 20:52:42 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,15 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "your-app-id"
+  apiKey: "AIzaSyBPTsWUgr8Rb7rpuncmb19wBbJkD422SPI",
+  authDomain: "resourcepack-bcbc8.firebaseapp.com",
+  projectId: "resourcepack-bcbc8",
+  storageBucket: "resourcepack-bcbc8.firebasestorage.app",
+  messagingSenderId: "568298057086",
+  appId: "1:568298057086:web:198684c8ac0a868119572f"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
 const auth = firebase.auth();
